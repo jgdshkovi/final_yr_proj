16e602dc803cab1da0e8161b2cc33dfa51d9d050
Jagadeesh <41447042+jgdshkovi@users.noreply.github.com>
2021-06-11 19:04:25 +0530

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,8 @@
+ckey = 'consumer_key'
+csecret = 'consumer_secret_key'
+bearer_token = 'bearer_token'
+
+access_token = 'access_token'
+access_token_secret = 'access_token_secret'
+#you get these keys once you signup for twitter dev account 
+#to get access to their API using these keys :)
