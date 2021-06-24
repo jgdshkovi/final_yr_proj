af4709f61b920e7f39ef04071ee9dd128b090fe8
Jagadeesh <41447042+jgdshkovi@users.noreply.github.com>
2021-06-24 09:05:56 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,8 +1,15 @@
-ckey = 'consumer_key'
-csecret = 'consumer_secret_key'
-bearer_token = 'bearer_token'
+#ckey = 'consumer_key'
+#csecret = 'consumer_secret_key'
+#bearer_token = 'bearer_token'
 
-access_token = 'access_token'
-access_token_secret = 'access_token_secret'
+#access_token = 'access_token'
+#access_token_secret = 'access_token_secret'
 #you get these keys once you signup for twitter dev account 
 #to get access to their API using these keys :)
+
+ckey = 'lTDEXAARTWU2xpI7gAc5M48qR'
+csecret = '58OBYz9DSvp8FKxb4e1yEWuCeJYcc0zMUo01e7NvWSx2JzaAGe'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAEVrLAEAAAAAEaqIKtJYFxIWIhQOGnAiLpCGZU8%3DU9J4Ajc8O7T0ibGnivAH3liptrxnrceR9obuz4x6d2bfuHsrsH'
+
+access_token = '849660043985379328-eHGpzocKI864wyu2dXMcXdmjRNo3VIi'
+access_token_secret = 'pN3kaXnMQ9d5ZCKzEJXg2Dzo8XbKawWwk6li3JXB40Dv9'
